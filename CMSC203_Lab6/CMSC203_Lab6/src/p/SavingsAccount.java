acedfad1caa320018a1144742d563deace11e70b
Gianpaulo_Cruz <cruzgianpaulo@gmail.com>
2024-04-03 20:55:12 -0400

    Images

---
+++ CMSC203_Lab6/CMSC203_Lab6/src/p/SavingsAccount.java
@@ -0,0 +1,30 @@
+package p;
+
+public class SavingsAccount extends BankAccount {
+	private static double rate = 0.025;
+	private int savingsNumber = 0;
+	private String accountNumber;
+	
+	public SavingsAccount(String name, double initialB) {
+		super(name, initialB);
+		savingsNumber++;
+		accountNumber = super.getAccountNumber() + "-" + savingsNumber;
+	}
+	
+	public void postInterest() {
+		double interest = getBalance() * rate / 12;
+		deposit(interest);
+	}
+	
+	@Override
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+	
+	public SavingsAccount(SavingsAccount originalAccount,double initialB) {
+		super(originalAccount, initialB);
+		savingsNumber = originalAccount.savingsNumber + 1;
+		accountNumber = super.getAccountNumber() + "-" + savingsNumber;
+	}
+	
+}
--- Lab/DriverClass2.java
+++ Lab/DriverClass2.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class DriverClass2 extends Movie{
+public class DriverClass extends Movie{
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
